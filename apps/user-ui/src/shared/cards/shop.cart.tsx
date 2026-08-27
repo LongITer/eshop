5ccc89286248b713edbf27d1f50dea14366f97b9
Long <longiter04@gmail.com>
2026-08-27 15:35:39 +0700

    style: reduce cover image height in shop card

--- apps/user-ui/src/shared/cards/shop.cart.tsx
+++ apps/user-ui/src/shared/cards/shop.cart.tsx
@@ -25,9 +25,9 @@ const ShopCard: React.FC<ShopCardProps> = ({ shop }) => {
           "https://thiepmung.com/images/theme/ngam-va-tai-hinh-nen-dep-cho-may-tinh-556444149b46f6.jpg"
         }
         alt="Cover"
-        width={500}
-        height={500}
-        className="object-cover w-full h-full"
+        width={400}
+        height={400}
+        className="object-cover w-full h-32"
       />
 
       {/* Shop Avatar */}
@@ -44,6 +44,14 @@ const ShopCard: React.FC<ShopCardProps> = ({ shop }) => {
             className="object-cover"
           />
         </div>
+      </div>
+
+      {/* Information */}
+      <div className="px-4 pb-4 pt-2 text-center">
+        <h3 className="text-base font-semibold text-gray-800">{shop.name}</h3>
+        <p className="text-xs text-gray-500 mt-0.5">
+          {shop?.followers?.length ?? 0} Followers
+        </p>
       </div>
     </div>
   );
